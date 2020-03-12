e8ccc0d323fa8d1f7d2e3d432e6f139db9f209e2
nicolasforeros <47544944+nicolasforeros@users.noreply.github.com>
2020-03-12 18:23:27 -0500

    Navigation v1.0

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,25 +1,13 @@
 import * as React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Platform} from 'react-native';
 import { Icon } from 'react-native-elements'
+import { StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-const data = [
-  {id:1, text: "CHECKLIST"},
-  {id:2, text: "COMBUSTIBLE"},
-  {id:3, text: "OTROS GASTOS"},
-  {id:4, text: "NOVEDADES"},
-]
 
 function Home(){
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <FlatList
-        data={data}
-        keyExtractor={(item) => (item.item)}
-        ItemSeparatorComponent={() => <View style={styles.ItemSeparator}></View>}
-        contentContainerStyle ={{borderBottomColor: 'grey', borderBottonWidth:1 }}
-        renderItem={({item, index}) => <listItem item={item} index={index}/>}
-      />
+      <Text>SCREEN HOME</Text>
     </View>
   );  
 }
@@ -28,35 +16,36 @@ const Stack = createStackNavigator();
 
 export default function HomeScreen({ navigation }) {
   return (
-      <Stack.Navigator>
-        <Stack.Screen 
-          name="Home" 
-          component={Home}
-          options={{
-            title: 'Home',
-            headerLeft: () => (
-              <TouchableOpacity 
-                style= {styles.MenuStyle}
-                onPress={() => navigation.toggleDrawer()}
-                hitSlop={{top: 20, bottom: 20, left: 50, right: 50}}>
-                <Icon
-                  name='bars'
-                  type='font-awesome'
-                  color='#FFFFFF'
-                />
-              </TouchableOpacity>
-            ),
-            headerStyle: {
-              backgroundColor: '#E0A729',
-              height: Platform.OS === 'ios' ? 105 : 85
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-            headerTitleAlign: 'center'
-          }}/>
-      </Stack.Navigator>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home" 
+        component={Home}
+        options={{
+          title: 'Home',
+          headerLeft: () => (
+            <TouchableOpacity 
+              style= {styles.MenuStyle}
+              onPress={() => navigation.toggleDrawer()}
+              hitSlop={{top: 20, bottom: 20, left: 50, right: 50}}>
+              <Icon
+                name='bars'
+                type='font-awesome'
+                color='#FFFFFF'
+              />
+            </TouchableOpacity>
+          ),
+          headerStyle: {
+            backgroundColor: '#E0A729',
+            height: Platform.OS === 'ios' ? 105 : 85
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerTitleAlign: 'center'
+        }}
+      />   
+    </Stack.Navigator>
   );
 }
 
